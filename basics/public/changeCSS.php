0b02ac3da5f3428d33b4db9ed4ac240bd8dc8358
Virgil Anderson <virgil.anderson@gmail.com>
2018-06-24 12:15:04 -0700

    Added the show element demo to the changeCSS.php page

--- basics/public/changeCSS.php
+++ basics/public/changeCSS.php
@@ -6,9 +6,15 @@
         <main>
           <h1>Change CSS Values</h1>
           <p>javaScript can easily change style values to enhance the look of your web project</p>
+          <h2>Change font style</h2>
           <p class="code_example">document.getElementById("demo").style.fontSize ="35px"; </p>
           <p id="demo">Watch the text change before your very eyes</p>
           <button onclick="document.getElementById('demo').style.fontSize='35px'">Click Me</button>
+          <h2>Show Elements</h2>
+          <p>javScript can dynamically display hidden elements on the page at any time</p>
+          <p id="code_example">getElementById("demo_2").style.display ="block"; </p>
+          <p id="demo_2" style="display: none">TAA DAAAA!!!!!</p>
+          <button onclick="document.getElementById('demo_2').style.display='block'">Want to see a magic trick?</button>
 
         </main>
         <?php INCLUDE(SHARED_PATH . '/aside.php'); ?>
