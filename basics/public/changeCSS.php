d9c8ee6780a93bdabf08f578641f89ec832f3d6a
Virgil Anderson <virgil.anderson@gmail.com>
2018-06-24 12:20:04 -0700

    Added the hide element css demo on the changeCSS.php page

--- basics/public/changeCSS.php
+++ basics/public/changeCSS.php
@@ -15,7 +15,11 @@
           <p id="code_example">getElementById("demo_2").style.display ="block"; </p>
           <p id="demo_2" style="display: none">TAA DAAAA!!!!!</p>
           <button onclick="document.getElementById('demo_2').style.display='block'">Want to see a magic trick?</button>
-
+          <h2>Hide Elements</h2>
+          <p>javaScript can also dynamically hide elements</p>
+          <p id="code_example">getElementById("demo_3").style.display="none";</p>
+          <p id="demo_3">Watch me disappear!</p>
+          <button onclick="getElementById('demo_3').style.display='none'">Abra Cadabra</button>
         </main>
         <?php INCLUDE(SHARED_PATH . '/aside.php'); ?>
       </article>
