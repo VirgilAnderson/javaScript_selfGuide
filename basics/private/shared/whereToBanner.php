c579c22cc1360a11bddac012fa1fd49055b803a4
Virgil Anderson <virgil.anderson@gmail.com>
2018-06-24 12:45:31 -0700

    Added the whereToBanner.php page

---
+++ basics/private/shared/whereToBanner.php
@@ -0,0 +1,9 @@
+<div class="row">
+  <article>
+      <section>
+        <h3>Wondering where you should write your javaScript?</h3>
+        <p>You can change HTML content, HTML attribute values, CSS values and you can show or hide elements!</p>
+        <p>JavaScript ROCKS!!!</p>
+      </section>
+  </article>
+</div><!-- .row -->
