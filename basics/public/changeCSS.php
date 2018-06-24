e032c824ee4306e4cd2459b732c4816673556ba2
Virgil Anderson <virgil.anderson@gmail.com>
2018-06-24 12:02:51 -0700

    Added CSS style change demo

--- basics/public/changeCSS.php
+++ basics/public/changeCSS.php
@@ -5,7 +5,10 @@
       <article>
         <main>
           <h1>Change CSS Values</h1>
-          
+          <p>javaScript can easily change style values to enhance the look of your web project</p>
+          <p class="code_example">document.getElementById("demo").style.fontSize ="35px"; </p>
+          <p id="demo">Watch the text change before your very eyes</p>
+          <button onclick="document.getElementById('demo').style.fontSize='35px'">Click Me</button>
 
         </main>
         <?php INCLUDE(SHARED_PATH . '/aside.php'); ?>
