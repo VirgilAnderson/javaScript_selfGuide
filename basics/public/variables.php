06f01bafc7f2ba2dd15c9b8b4732392bcc5392d2
Virgil Anderson <virgil.anderson@gmail.com>
2018-06-24 21:44:03 -0700

    Added the assignment operator code example to the variables.php page

--- basics/public/variables.php
+++ basics/public/variables.php
@@ -6,12 +6,17 @@
         <main>
           <h1>javaScript Variables</h1>
           <p>Variables give javaScript life!</p>
+
           <h2>Variable Identifiers</h2>
           <p>In order to create a variable, you must first give it a name. Let's name our variable box_1. It is useful however to give your variables a meaningul name.</p>
           <p>Declare your variable with the keyword var</p>
           <p class="code_example">var box_1;</p>
 
-          
+          <h2>Assignment operator</h2>
+          <p>To put information into your variable container, we must use the assignment operator. The assignment operator is the = sign.</p>
+          <p class="code_example">var box_1 = 8;</p>
+          <p>Now our box stores the number 8. We can use this information to output the number later.</p>
+
 
         </main>
         <?php INCLUDE(SHARED_PATH . '/aside.php'); ?>
