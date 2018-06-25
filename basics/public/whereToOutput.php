7c8693cabeb8d1186f1ab70149e5e726bc4107c7
Virgil Anderson <virgil.anderson@gmail.com>
2018-06-24 20:50:52 -0700

    Added the document write code example to whereToOutput.php

--- basics/public/whereToOutput.php
+++ basics/public/whereToOutput.php
@@ -6,10 +6,18 @@
         <main>
           <h1>Where Should I output my javaScript?</h1>
           <p>You may be wondering where you can print your data out with javaScript. We'll start by embedding it in HTML</p>
+
           <h2>Outputting javaScript to HTML</h2>
           <p class="code_example">document.getElementById("sample").innerHTML= 5 + 6;</p>
           <p id="sample">Here is the text to change</p>
           <button type="button" onclick="document.getElementById('sample').innerHTML= 5 + 6;">What's 5 + 6</button>
+
+          <h2>Outputting with document write</h2>
+          <p>You can write directly into the document with this method but be careful, using this method after the html has loaded will delete all the html</p>
+          <p class="code_example">document.write(5 + 6);</p>
+          <script>document.write(5 + 6);</script>
+
+          
         </main>
         <?php INCLUDE(SHARED_PATH . '/aside.php'); ?>
       </article>
