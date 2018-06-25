ea5a66385a4bb4856df7c34bb288b2deb6120a48
Virgil Anderson <virgil.anderson@gmail.com>
2018-06-24 21:37:54 -0700

    Added javascript variables code example

--- basics/public/variables.php
+++ basics/public/variables.php
@@ -4,7 +4,14 @@
 <?php INCLUDE(SHARED_PATH . '/variableBanner.php'); ?>
       <article>
         <main>
-          <h1>
+          <h1>javaScript Variables</h1>
+          <p>Variables give javaScript life!</p>
+          <h2>Variable Identifiers</h2>
+          <p>In order to create a variable, you must first give it a name. Let's name our variable box_1. It is useful however to give your variables a meaningul name.</p>
+          <p>Declare your variable with the keyword var</p>
+          <p class="code_example">var box_1;</p>
+
+          
 
         </main>
         <?php INCLUDE(SHARED_PATH . '/aside.php'); ?>
