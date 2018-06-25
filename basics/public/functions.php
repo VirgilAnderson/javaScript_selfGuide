1978d2fe37ac41e52f813ffe35678d4b6256d43d
Virgil Anderson <virgil.anderson@gmail.com>
2018-06-25 13:56:46 -0700

    Added the basic function structure code example to functions.php

--- basics/public/functions.php
+++ basics/public/functions.php
@@ -4,6 +4,14 @@
 <?php INCLUDE(SHARED_PATH . '/functionsBanner.php'); ?>
       <article>
         <main>
+          <h1>JavaScript Functions</h1>
+          <p>Any developer worth their salt will possess a mastery of functions. A function can make changes to your page or it can calculate and return data.</p>
+
+          <h2>Basic function structure</h2>
+          <p>A function includes a declaration of function, a name, parameters and a set of instructions.</p>
+          <p class="code_example">function name(parameter, parameter2){</p>
+          <p class="code_example"> code to be executed</p>
+          <p class="code_example">}</p>
 
         </main>
         <?php INCLUDE(SHARED_PATH . '/aside.php'); ?>
