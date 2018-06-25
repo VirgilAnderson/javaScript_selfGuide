0b46b156519fa51c0bbd7b025daf5931e33ba486
Virgil Anderson <virgil.anderson@gmail.com>
2018-06-24 21:32:05 -0700

    Created variables.php

---
+++ basics/private/shared/variableBanner.php
@@ -0,0 +1,9 @@
+<div class="row">
+  <article>
+      <section>
+        <h3>javaScript Variables!</h3>
+        <p>It's useful to think of a variable as an empty box in which you can add information. In javaScript we can create variables to store numbers, writing and even boolean conditions like true/false.</p>
+        <p>Now it's getting interesting!</p>
+      </section>
+  </article>
+</div><!-- .row -->
---
+++ basics/public/variables.php
@@ -0,0 +1,14 @@
+<?php require_once('../private/initialize.php'); ?>
+<?php $page = "Variables in javaScript"; // Change the page name?>
+<?php INCLUDE(SHARED_PATH . '/header.php'); ?>
+<?php INCLUDE(SHARED_PATH . '/variableBanner.php'); ?>
+      <article>
+        <main>
+          <h1>
+
+        </main>
+        <?php INCLUDE(SHARED_PATH . '/aside.php'); ?>
+      </article>
+    </div><!-- .row -->
+    <script>var page = "link_6"; // assign the class .active to the active page</script>
+    <?php INCLUDE(SHARED_PATH .'/footer.php'); ?>
