795e8ee528b29592ba26803652f4fd9968c29b5f
Virgil Anderson <virgil.anderson@gmail.com>
2018-06-26 13:11:09 -0700

    Added string length code example on stringMethods.php

--- basics/public/stringMethods.php
+++ basics/public/stringMethods.php
@@ -6,6 +6,18 @@
         <main>
           <h1>Using String Methods</h1>
           <p>Strings as objects allows you to find out how many characters are contained within your string, search for specific content within your string or even extract bits out of a string.</p>
+          <h2>String Length</h2>
+          <p>A useful built in feature within javaScript is the string length function. It enables you to take a string and determine how long the string is.</p>
+          <div  class="code_example">
+            <p>var sampleText = "Mary had a little lamb";</p>
+            <p>var result = sampleText.length;</p>
+          </div>
+          <p id="demo">How many letters are in variable sampleText?</p>
+          <script>
+            var sampleText="Mary had a little lamb";
+            var result = sampleText.length;
+          </script>
+          <button onclick="document.getElementById('demo').innerHTML = result">Click Here To Find Out</button>
           <?php INCLUDE(SHARED_PATH . '/pagination.php'); ?>
         </main>
         <?php INCLUDE(SHARED_PATH . '/aside.php'); ?>
