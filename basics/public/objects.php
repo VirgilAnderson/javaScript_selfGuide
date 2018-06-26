be35e5b2b991680b5228c3c969baad0866adb92c
Virgil Anderson <virgil.anderson@gmail.com>
2018-06-25 22:46:03 -0700

    Added the method code example to objects.php

--- basics/public/objects.php
+++ basics/public/objects.php
@@ -20,8 +20,33 @@
           </script>
           <button type="button" onclick="document.getElementById('demo').innerHTML = 'The model of this ' + aircraft.type + ' is ' + aircraft.model + ' and its color is ' + aircraft.color + '.'">Click here</button>
 
-
-
+          <h2>Object Methods</h2>
+          <p>In addition to properties, objects can possess methods. A method is a function stored as a property inside the object.</p>
+          <p>To use a method, we use the keyword THIS.</p>
+          <div class="code_example">
+            <p>var relative = {</p>
+              <p class="indent">firstName: "David",</p>
+              <p class="indent">lastName: "Anderson",</p>
+              <p class="indent">relation: "uncle",</p>
+              <p class="indent">fullName: function(){</p>
+                <p class="indent">return this.firstName + " " + this.lastName;</p>
+              <p class="indent">}</p>
+            };</p>
+          </div>
+          <p>To access the data, simply call the function.<p>
+          <p class="code_example">document.getElementById('test').innerHTML = relative.fullName();</p>
+          <p id="test">Here</p>
+          <script>
+            var relative = {
+              firstName: "David",
+              lastName: "Anderson",
+              relation: "uncle",
+              fullName: function(){
+                return this.firstName + " " + this.lastName;
+              }
+            };
+          </script>
+          <button type="button" onclick="document.getElementById('test').innerHTML = relative.fullName();">Click Me</button>
         </main>
         <?php INCLUDE(SHARED_PATH . '/aside.php'); ?>
         <?php INCLUDE(SHARED_PATH . '/pagination.php'); ?>
