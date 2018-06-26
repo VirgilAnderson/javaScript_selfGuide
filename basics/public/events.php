3aa2f05813d75b25bdbb3e52fcdd4727ebb41486
Virgil Anderson <virgil.anderson@gmail.com>
2018-06-25 23:55:34 -0700

    added the function code example to events.php

--- basics/public/events.php
+++ basics/public/events.php
@@ -6,18 +6,32 @@
         <main>
           <h1>Events</h1>
           <p>javaScript empowers you to utilize different user actions to trigger your functions. You can trigger your functions when the page loads, when the user clicks on something, hovers over something, when they press a key, when they move their cursor away from something or even when an html element has changed.</p>
+          
           <h2>Syntax</h2>
           <p>Regardless of the event you choose to target, your syntax will look similar.</p>
           <p class="code_example">&ltelement event="Your code here"&gt</p>
+
           <h2>onClick</h2>
           <p>Here is how that code would look utilizing a button click</p>
           <p class="code_example">&ltbutton onclick="document.getElementById('test').innerHTML = 'foo'"&gtClick Me&ltbutton&gt</p>
           <p id="test"></p>
           <button onclick="document.getElementById('test').innerHTML='foo'">Click Me</button>
+
           <h2>onMouseOver</h2>
           <p>The process is similar with all the events you want to use. Follow the same syntax and it should work just fine. onMouseOver works when the user moves their cursor off of an element.</p>
           <p class="code_example">&ltp onmouseover="this.style.fontSize = '35'"&gtFeed Me Seamore!&lt/p&gt</p>
           <p onmouseover="this.style.fontSize='34px'">Feed Me Seamore!</p>
+
+          <h2>Function</h2>
+          <p>Because of the length of javaScript code, it's more common to see a function placed inside the event call syntax.</p>
+          <p class="code_example">&ltinput onkeydown="myFunction()"&gt</p>
+          <input onkeydown="myFunction()">
+          <p id="example">Type in the field</p>
+          <script>
+            function myFunction(){
+              document.getElementById('example').innerHTML = "You typed in my field!";
+            }
+          </script>
 
           <?php INCLUDE(SHARED_PATH . '/pagination.php'); ?>
         </main>
